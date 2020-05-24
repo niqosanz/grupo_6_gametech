cbd651fdb172d35f8995225d16fe3a8129da29db
Lorenok <nokubi.lorena@gmail.com>
2020-05-24 15:55:50 -0300

    archivo de rutas de usuarios con las ultimas modificaciones

--- gametech/routes/users.js
+++ gametech/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var usersController = require('../controllers/usersController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/create', usersController.create);
 
 module.exports = router;
