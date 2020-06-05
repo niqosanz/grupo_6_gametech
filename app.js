39e4728f60b432aa3623ea0be2929c9bad9738a7
Javier Suarez <javiersuarezdominguez@gmail.com>
2020-06-05 17:01:16 -0300

    direccionar icono user en home, a ruta register

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use('/register', registerRouter);
 
 app.use('/users/admin', usersRouter)
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
