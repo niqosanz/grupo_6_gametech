9be8599774b92c2fc158500ee92ad53c34c3d4fc
Javier Suarez <javiersuarezdominguez@gmail.com>
2020-06-05 23:16:09 -0300

    Validaciones Registro y redireccionamiento en caso de no estar registrado el usuario o no coincidir el email

--- routes/register.js
+++ routes/register.js
@@ -4,8 +4,26 @@ let fs = require('fs');
 
 /* GET Register. */
 router.get('/', function(req, res, next) {
-  res.render('register',{pageCss: 'register.css'});
+  res.render('register',{pageCss: 'register.css',statusRegistracion: ''});
 });
+
+
+
+/* GET Register. */
+router.get('/:id', function(req, res, next) {
+  
+    let statusRegistroUsuario = req.params.id;
+
+    if(statusRegistroUsuario){
+      res.render('register',{pageCss: 'register.css',statusRegistracion: 'Usuario o Contraseña incorrecta.'});
+    }else{
+      res.render('register',{pageCss: 'register.css',statusRegistracion: ''});
+
+    }
+
+});
+
+
 
 /* GET Validation. */
 router.post('/', function(req, res, next) {
@@ -13,8 +31,21 @@ router.post('/', function(req, res, next) {
     let users = fs.readFileSync('data/DBUsers.json',{encoding:'utf-8'});
     let usersJSON = JSON.parse(users);
 
-    res.send('usuario: ' + req.body.email + ' ' +'password: ' + req.body.password);
+    for(let i = 0; i<usersJSON.length; i++){
 
-  });
+      if(req.body.email == usersJSON[i].email && req.body.password == usersJSON[i].password ){
+        res.send('Usuario Existente');
+
+      }else{
+
+        res.redirect('/register/1');
+      }
+
+    }
+
+});
+
+
+
 
 module.exports = router;
