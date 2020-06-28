bdded0be0190ae6b991a43e196003e1fbbc1bfea
Lorenok <nokubi.lorena@gmail.com>
2020-06-27 23:40:50 -0300

    Agrego validacion desde el front para el campo description

--- public/validFrontAdd.js
+++ public/validFrontAdd.js
@@ -5,7 +5,6 @@ console.log(form)
 form.name.addEventListener('keyup', function(event){
     let mensaje = event.target.value;
     event.target.classList.add('is-invalid')
-    form.submit.classList.add('disabled')
     
     if(mensaje.length <5){
         console.log(event.target.value)
@@ -18,4 +17,18 @@ form.name.addEventListener('keyup', function(event){
 }
 
 });
+
+form.description.addEventListener('keyup', function(event){
+    let mensaje = event.target.value;
+    event.target.classList.add('is-invalid')
     
+    if(mensaje.length <20){
+        event.target.classList.replace('is-valid','is-invalid');
+
+}else {
+    event.target.classList.replace('is-invalid','is-valid') ;
+    
+
+}
+
+});
