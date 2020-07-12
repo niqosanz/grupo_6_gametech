eca18b5e45dabf0561b34e204ad1246e747647c8
Lorenok <nokubi.lorena@gmail.com>
2020-07-12 16:19:52 -0300

    correccion de las validaciones del front de productos

--- public/validFrontAdd.js
+++ public/validFrontAdd.js
@@ -1,11 +1,24 @@
+window.addEventListener('load', function() {
+    let formulario = document.querySelector('form');
+    formulario.addEventListener('submit', function(evento) {
+        console.log(formulario)
+  
+    })
+});
+
+
+
 var form = document.getElementById('addProductForm')
 var buttonSend = document.querySelector('#sendInformation');
+var images = document.querySelector('#imagen')
+buttonSend.disabled =true;
 
-console.log(buttonSend.classList)
+
 
 form.name.addEventListener('keyup', function(event){
     event.target.classList.add('is-invalid')
     buttonSend.disabled =true;
+    console.log(buttonSend.classList)
     
     let mensaje = event.target.value;
     
@@ -24,6 +37,7 @@ form.name.addEventListener('keyup', function(event){
 
 form.description.addEventListener('keyup', function(event){
     let mensaje = event.target.value;
+    buttonSend.disabled =true;
 
     event.target.classList.add('is-invalid')
     
@@ -32,6 +46,7 @@ form.description.addEventListener('keyup', function(event){
 
 }else {
     event.target.classList.replace('is-invalid','is-valid') ;
+    buttonSend.disabled =false;
     
 
 }
