8609623029c02d9eb8e85d8df28882e16745af69
Lorenok <nokubi.lorena@gmail.com>
2020-07-12 23:00:02 -0300

    Agrego detalles en la api de productos

--- controllers/api/apiProductController.js
+++ controllers/api/apiProductController.js
@@ -5,12 +5,30 @@ var db = require('../../db/models');
 const controller ={
     list: function (req,res) {
         db.Product.findAll({include:[{association:"brand"},{association:"category"}]}).then(function (productos){
-            ;let respuesta ={
-               
-                    count: productos.length, 
-                    productos,
-
+            ;
+            var products = [];
+            for (i=0; i<productos.length ;i++){
                 
+                let idProduct = productos[i].id;
+                let nameProduct = productos[i].short_description;
+                let descriptionProduct = productos[i].long_description;
+                let categorias = productos[14].category.dataValues.name;
+                let detailProduct ='http://localhost:3000/api/products/'+idProduct;
+                
+                var newProducts = {'id':idProduct,'name':nameProduct,'description':descriptionProduct,'categories':categorias, 'detail': detailProduct}
+                products.push(newProducts)
+
+                i++
+
+            }
+            
+           
+   
+            let respuesta ={
+            
+                    count: productos.length,
+                    productos: products
+                    
             }
 
             res.json(respuesta)
