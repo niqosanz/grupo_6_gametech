0692eb4973c9ab8c33b89fd628c88179aa3716a3
niqosanz <nicosanchez.web01@gmail.com>
2020-07-19 16:02:18 -0300

    merge

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var multer = require ('multer')
 let path = require('path')
 var apiProductController = require('../../controllers/api/apiProductController')
 var app = express();
@@ -8,6 +7,7 @@ var app = express();
 
 /* GET products listing. */
 router.get('/', apiProductController.list);
+router.get('/:id?', apiProductController.detail)
 
 
 
