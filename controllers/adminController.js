d246aee4c4496a57e54d9bf9b9503c5c3bd83785
niqosanz <nicosanchez.web01@gmail.com>
2020-07-26 17:42:04 -0300

    cambios

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,20 @@
+var express = require('express');
+var db = require('../db/models');
+
+
+
 module.exports={
 
-    list: function (req,res){
-      res.render('admin')
-      },
+    // list: function (req,res){
+    //   res.render('admin')
+    //   },
+
+    list: function (req,res) {
+      db.Product.findAll({include:[{association:"brand"},{association:"category"}]}).then(function (resultados){
+        console.log(resultados);
+        res.render('admin',resultados)
+      },)
+    },
   
   }
   
