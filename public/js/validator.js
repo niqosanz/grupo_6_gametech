b574160dbdd671221072210a3044809f2d999fa8
Lorenok <nokubi.lorena@gmail.com>
2020-07-28 17:17:55 -0300

    Agrego la validacion desde el front en la creación de productos y la información que envío a la vista del Home

---
+++ public/js/validator.js
@@ -0,0 +1,54 @@
+window.addEventListener('load', function() {
+    let formulario = document.querySelector('form');
+    formulario.addEventListener('submit', function(evento) {
+        console.log(formulario)
+  
+    })
+});
+
+
+
+var form = document.getElementById('addProductForm')
+var buttonSend = document.querySelector('#sendInformation');
+var images = document.querySelector('#imagen')
+buttonSend.disabled =true;
+
+
+
+form.name.addEventListener('keyup', function(event){
+    event.target.classList.add('is-invalid')
+    buttonSend.disabled =true;
+    console.log(buttonSend.classList)
+    
+    let mensaje = event.target.value;
+    
+    
+    if(mensaje.length <5){
+        buttonSend.disabled =true;
+
+}else {
+    event.target.classList.replace('is-invalid','is-valid'),
+    buttonSend.disabled =false;
+    
+
+}
+
+});
+
+form.description.addEventListener('keyup', function(event){
+    let mensaje = event.target.value;
+    buttonSend.disabled =true;
+
+    event.target.classList.add('is-invalid')
+    
+    if(mensaje.length <20){
+        event.target.classList.replace('is-valid','is-invalid');
+
+}else {
+    event.target.classList.replace('is-invalid','is-valid') ;
+    buttonSend.disabled =false;
+    
+
+}
+
+});
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/models');
 
 /* GET home page. */
 router.get('/', function(req, res){
-  if(req.cookies.recordame == undefined){
-      res.render ('index',{usuario: ''});
+  db.Product.findAll({include:[{association:"brand"},{association:"category"}]}).then(function (resultados){
+    let productos = resultados 
+    if(req.cookies.recordame == undefined){ 
+      res.render ('index',{usuario: '', productos});
   }else {
-    res.render ('index',{usuario: req.cookies.recordame});
+    res.render ('index',{usuario: req.cookies.recordame, productos });
 
   }
+    
+},
+)
+  
+  
 });
 router.get('/pruebaSession', function(req,res){
   if (req.session.numeroVisitas == undefined){
