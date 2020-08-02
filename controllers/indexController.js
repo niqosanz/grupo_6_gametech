60fea578607be891c58fdd584ca2e90e862e4773
niqosanz <nicosanchez.web01@gmail.com>
2020-08-02 14:47:36 -0300

    push

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,96 +1,111 @@
-var express = require('express');
-var db = require('../db/models');
-var Sequelize = require('sequelize');
+var express = require("express");
+var db = require("../db/models");
+var Sequelize = require("sequelize");
 //const { notIn } = require('sequelize/types/lib/operators');
 //var op = Sequelize.Op;
 
-
-
-module.exports={
-    list: function(req, res){
-
-      db.Product.findAll(
-        {
-            where:{
-                categories_id1: 1,
-            }
+module.exports = {
+  list: function (req, res) {
+    db.Product.findAll(
+      {
+        where: {
+          categories_id1: 1,
+        },
+      },
+      { include: [{ association: "brand" }, { association: "category" }] }
+    ).then(function (productos) {
+      db.Category.findAll().then(function (categorias) {
+        // console.log(productos)
+        if (req.cookies.recordame == undefined) {
+          res.render("index", {
+            errors: "",
+            usuario: "",
+            productos,
+            categorias,
+          });
+        } else {
+          res.render("index", {
+            usuario: req.cookies.recordame,
+            productos,
+            categorias,
+          });
         }
-,{ include: [{ association: "brand" }, { association: "category" }] }
-    )   
-.then(function (productos){
-            db.Category.findAll().then(function(categorias){
-// console.log(productos)
-            if(req.cookies.recordame == undefined){ 
-                res.render ('index', {errors: '',usuario:'', productos, categorias });
-            }else {
-            res.render ('index',{usuario: req.cookies.recordame, productos,categorias});
-      }
-      })
+      });
+    });
+  },
+
+  preguntas: function (req, res) {
+    db.Product.findAll({
+      include: [{ association: "brand" }, { association: "category" }],
+    }).then(function (resultados) {
+      db.Category.findAll().then(function (categorias) {
+        let productos = resultados;
+        if (req.cookies.recordame == undefined) {
+          res.render("faq", { usuario: "", categorias, productos });
+        } else {
+          res.render("faq", {
+            usuario: req.cookies.recordame,
+            productos,
+            categorias,
+          });
+        }
+      });
+    });
+  },
+
+  search: function (req, res) {
+    var datoABuscar = req.query.query;
+    // console.log(req.query.query)
+
+    db.Product.findAll(
+      {
+        where: {
+          short_description: { [db.Sequelize.Op.substring]: "%" + datoABuscar },
+        },
+      },
+      { include: [{ association: "brand" }, { association: "category" }] }
+    ).then(function (productos) {
+      db.Category.findAll({
+        where: {
+          name: { [db.Sequelize.Op.substring]: "%" + datoABuscar },
+        },
+      }).then(function (categorias1) {
+        db.Brand.findAll({
+          where: {
+            name: { [db.Sequelize.Op.substring]: "%" + datoABuscar },
+          },
+        }).then(function (brand) {
+          db.Category.findAll().then(function (categorias) {
+            var resultados = [productos, brand, categorias1];
+            if (req.cookies.recordame == undefined) {
+              res.render("search", {
+                usuario: "",
+                categorias,
+                productos,
+                brand,
+                categorias1,
+                datoABuscar,
+              });
+            } else {
+              res.render("search", {
+                usuario: req.cookies.recordame,
+                productos,
+                categorias,
+                categorias1,
+                brand,
+                datoABuscar,
+              });
+            }
+          });
+        });
+      });
     })
-},
+    .catch(function (err) {
 
-      
-      preguntas: function(req, res){
+      res.redirect('/');
 
-        db.Product.findAll({include:[{association:"brand"},{association:"category"}]}).then(function (resultados){
-            db.Category.findAll().then(function(categorias){
-            let productos = resultados 
-            if(req.cookies.recordame == undefined){ 
-            res.render ('faq',{usuario: '',categorias, productos});
-      }else {
-            res.render ('faq',{usuario: req.cookies.recordame, productos,categorias});
-      }
-      })
-      
-      },
-      )
-      
-      },
-      
-
-
-
-
-
-
-      search: function(req, res){
-
-        var datoABuscar =req.query.query
-        // console.log(req.query.query)
-  
-          db.Product.findAll(
-            {where:{
-              short_description:{[db.Sequelize.Op.substring]: '%' + datoABuscar}
-            }},
-            {include:[{association:"brand"},{association:"category"}]})
-            .then(function (productos){
-              db.Category.findAll({where:{
-                name:{[db.Sequelize.Op.substring]: '%' + datoABuscar}
-              }}).then(function(categorias1){
-                db.Brand.findAll({where:{
-                  name:{[db.Sequelize.Op.substring]: '%' + datoABuscar}
-                }}).then(function(brand){
-                  db.Category.findAll().then(function(categorias){
-
-                    var resultados = [productos,brand,categorias1]
-    
-                    
-                    
-  
-        
-  
-              if(req.cookies.recordame == undefined){ 
-              res.render ('search',{usuario: '',categorias, productos,brand,categorias1, datoABuscar});
-                  }else {
-              res.render ('search',{usuario: req.cookies.recordame, productos,categorias,categorias1,brand, datoABuscar});
-                }
-                  })
-        
-               },
-                 )},
-        
-
-                  )
-                    },
-            )
-                }}
+      throw err;
+    })
+;
+  },
+};
