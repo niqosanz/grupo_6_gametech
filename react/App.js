3ca8e91db854d6ebeeda3d726517cddc8dfdb68d
Lorenok <nokubi.lorena@gmail.com>
2020-08-03 23:31:04 -0300

    ultimos cambios de react

---
+++ react/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+
+function App() {
+  return (
+    Sidebar()
+  );
+}
+
+export default App;
